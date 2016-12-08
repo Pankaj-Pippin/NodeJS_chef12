8815eaa8b9c65125c19c10ffd9211711d0733726
Pankaj-Pippin <pankaj@pippintechnologies.com>
2016-12-08 12:47:01 +0530

    chef debug

--- nodejs/recipes/start.rb
+++ nodejs/recipes/start.rb
@@ -6,7 +6,7 @@ bash 'download_git' do
   cwd '/opt'
   code <<-EOH
 	if [[  -e /opt/concreetadmin-server ]]; then
-		sudo rm -fr /opt/concreetadmin-server/
+		#sudo rm -fr /opt/concreetadmin-server
 	fi
 	sudo git clone  git@github.com:pippintech/concreetadmin-server.git
 	#dir ="/home/ec2-user/logs"
@@ -18,7 +18,6 @@ bash 'download_git' do
 		echo "$dir already exists but is not a directory" 1>&2
 	fi
   EOH
-  flags "-x"
 end
 
 
@@ -39,7 +38,6 @@ bash 'Killing_Node' do
 			sudo kill `pgrep node`
 		fi
   EOH
-  flags "-x"
 end
 
 Chef::Log.info 'Killed_Node'  
@@ -47,7 +45,7 @@ Chef::Log.info 'Killed_Node'
 # Copy the code to final location
 Chef::Log.warn 'Deploying code'
 
-bash 'deploy_Codex' do
+bash 'deploy_Code' do
   user 'root'
   cwd '/opt'
   code <<-EOH
@@ -61,9 +59,9 @@ bash 'deploy_Codex' do
 	cd /opt
 	sudo git clone  git@github.com:pippintech/concreetadmin-server.git
 	sudo git clone  git@github.com:pippintech/concreetadmin-server.git
-	sudo rsync -avzh --remove-source-files /opt/concreetadmin-server/ /home/ec2-user/cadmin/ > $LOGFILE
+	sudo rsync -avzh --remove-source-files /opt/concreetadmin-server/ /home/ec2-user/cadmin/
 	cd /home/ec2-user/cadmin/
-	sudo rm -fr /opt/concreetadmin-server/
+	#sudo rm -fr /opt/concreetadmin-server
   EOH
 end
 
@@ -84,7 +82,6 @@ bash 'Building_Code' do
 #	sudo nohup  grunt serve > /dev/null 2>&1 &
 	sudo nohup  gulp serve:dist > /dev/null 2>&1 &
   EOH
-  flags "-x"
 end
 
 
