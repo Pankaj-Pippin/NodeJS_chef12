85b25ded4df3d2e52921059bf822384ea7400856
Pankaj-Pippin <pankaj@pippintechnologies.com>
2016-12-08 15:25:38 +0530

    timestapms

--- nodejs/recipes/start.rb
+++ nodejs/recipes/start.rb
@@ -44,13 +44,13 @@ Chef::Log.info 'Killed_Node'
 
 # Copy the code to final location
 Chef::Log.warn 'Deploying code'
-
+ENV['TIMESTAMP']='`date "+%Y-%m-%d %H:%M:%S"`'
+ENV['TIMESTAMPS']=`date "+%Y-%m-%d %H:%M:%S"`
 bash 'deploy_Code' do
   user 'root'
   cwd '/opt'
   code <<-EOH
   	#mv /opt/concreetadmin-server/*.* /home/ec2-user/cadmin/
-	TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 	LOGFILE="/home/ec2-user/logs/rsync_$TIMESTAMP.log"
 	echo $LOGFILE
 	#export rbLOGFILE=$LOGFILE
